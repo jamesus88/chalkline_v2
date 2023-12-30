d57c61251942963b07e5f6142603a7e390ab065c
Aidan Hurwitz <aidan.hurwitz88@gmail.com>
2023-12-30 14:16:34 -0500

    v4.2.2

--- chalkline/invite/routes.py
+++ chalkline/invite/routes.py
@@ -6,14 +6,18 @@ invite = Blueprint('invite', __name__)
 
 @invite.route('/add-team/<teamId>')
 def add_team(teamId=None):
+    print(teamId)
     if not teamId:
         return redirect(url_for('main.home'))
     user = srv.getUser()
     if user is None:
-        session['next-page'] = 'invite.add_team'
+        session['next-url'] = request.path
         return redirect(url_for('main.login'))
     if 'coach' in user['role'] or 'parent' in user['role']:
-        db.addTeamToUser(user, teamId)
+        response = db.addTeamToUser(user, teamId)
+        if type(response) != str:
+            user = response
+            session['user'] = user
         
     return redirect(url_for('main.profile'))
 
