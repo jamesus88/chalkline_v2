e612f8276fea10ce18290896f4056899f98d21a3
Aidan Hurwitz <aidan.hurwitz88@gmail.com>
2024-02-20 16:06:59 -0500

    switch mail to tls

--- chalkline/__init__.py
+++ chalkline/__init__.py
@@ -9,11 +9,11 @@ def create_app():
     
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['MAIL_SERVER']='smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_PORT'] = 587
     app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') 
     app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-    app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
     
     mail.init_app(app)
     
