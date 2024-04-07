f9cc497c4edf17e5cc07bb0f89989a59365bd44c
Aidan Hurwitz <aidan.huritz88@gmail.com>
2024-04-07 16:30:26 -0400

    fixed timezones!

--- chalkline/calendar_srv.py
+++ chalkline/calendar_srv.py
@@ -1,21 +1,17 @@
-import icalendar, datetime, pytz
+import icalendar, datetime, x_wr_timezone, pytz
 from chalkline import get_events, server as srv
 
 def add_event(cal: icalendar.Calendar, event, role):
     cal_event = icalendar.Event()
     cal_event.add('uid', str(event['_id']))
-    cal_event.add('dtstart', event['eventDate'])
 
-    #convert to utc
-    event['eventDate'] = event['eventDate'].replace(tzinfo=pytz.timezone('EST'))
+    cal_event.add('dtstart', event['eventDate'])
 
     if event['eventAgeGroup'] in ['Majors', '50/70', 'Juniors']:
         game_length = datetime.timedelta(hours=2)
     else:
         game_length = datetime.timedelta(hours=1.5)
     cal_event.add('dtend', event['eventDate'] + game_length)
-    cal_event.add('tzoffsetfrom', datetime.timedelta(hours=-4))
-    cal_event.add('tzoffsetto', datetime.timedelta(hours=-5))
 
 
     cal_event.add('summary', f"{event['awayTeam']} @ {event['homeTeam']} ({role}) - Chalkline")
@@ -34,6 +30,16 @@ def serve_calendar(user, start_date=None):
     '''
     calendar = icalendar.Calendar()
     calendar.add("prodid", "Chalkline Baseball")
+    calendar.add('calscale', 'Gregorian')
+    calendar.add('version', '2.0')
+    calendar.add('x-wr-timezone', 'America/New_York')
+
+    tz = icalendar.Timezone()
+    tz.add('tzid', 'America/New_York')
+    
+    calendar.add_component(tz)
+    
+    
     eventList = []
 
     eventFilter = get_events.EventFilter()
@@ -59,8 +65,4 @@ def serve_calendar(user, start_date=None):
             add_event(calendar, event, 'Away Team')
             continue
 
-    return calendar.to_ical()
-
-
-
-
+    return x_wr_timezone.to_standard(calendar).to_ical()
