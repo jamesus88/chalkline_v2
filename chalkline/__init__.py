a2cc29253296ef4c87a8281de4f19251b1335ce9
jamesus88 <aidan.hurwitz88@gmail.com>
2025-06-16 10:38:14 -0400

    lengthened session time to 2 hours

--- chalkline/__init__.py
+++ chalkline/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
-    app.config['PERMANENT_SESSION_LIFETIME'] =  datetime.timedelta(minutes=30)
+    app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(hours=2)
     
     mail.init_app(app)
     
